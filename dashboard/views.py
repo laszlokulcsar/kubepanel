b6570f6350cc341385c3dea6119dd90c3c5d72b6
Laszlo Kulcsar <laszlokulcsar86@gmail.com>
2024-12-28 12:40:10 +0100

    modified:   dashboard/views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -9,7 +9,7 @@ from cryptography.hazmat.primitives import serialization as crypto_serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend as crypto_default_backend
 
-import os, random, base64, string
+import os, random, base64, string, requests
 
 TEMPLATE_BASE = "/kubepanel/dashboard/templates/"
 
