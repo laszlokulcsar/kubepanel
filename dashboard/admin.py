59d9685da847de86e0a9591b107448e9fd7bce3b
Laszlo Kulcsar <laszlokulcsar86@gmail.com>
2025-01-06 11:13:25 +0100

    modified:   dashboard/admin.py

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Domains, Volumesnapshot
+from .models import Domains, Volumesnapshot, BlockRule
 
 admin.site.register(Domains)
 admin.site.register(Volumesnapshot)
