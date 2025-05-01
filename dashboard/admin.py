33d168f7305c0d155c71fe05dd7cf0f1a0c372e2
Laszlo Kulcsar <laszlokulcsar86@gmail.com>
2025-05-01 10:46:48 +0200

    modified:   admin.py 	modified:   templates/yaml_templates/ingress_base.yaml

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import MailUser, ClusterIP, Domain, Volumesnapshot, BlockRule, DNSRecord, DNSZone, CloudflareAPIToken
 
 admin.site.register(Domain)
+admin.site.register(DomainAlias)
 admin.site.register(Volumesnapshot)
 admin.site.register(BlockRule)
 admin.site.register(DNSRecord)
