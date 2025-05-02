328a28c07774106f388fafe4a9d35797bcd627a7
Laszlo Kulcsar <laszlokulcsar86@gmail.com>
2025-05-02 15:06:02 +0200

    modified:   forms.py

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,4 +1,5 @@
 from django import forms  
+from django.core.exceptions import ValidationError
 from django.contrib.auth.hashers import make_password
 from dashboard.models import Domain, DomainAlias, CloudflareAPIToken, DNSRecord, DNSZone, MailUser, MailAlias
 from passlib.hash import sha512_crypt
